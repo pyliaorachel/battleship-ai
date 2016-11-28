d4279ca2b53d5902246ab5a9266d4bd6096cd811
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2016-11-27 22:33:21 -0500

    propagators & orderings

--- csp/propagators.py
+++ csp/propagators.py
@@ -9,6 +9,7 @@ propagator == a function with the following template
 Consider implementing propagators for forward cehcking or GAC as a course project!        
 
 '''
+from collections import deque
 
 def prop_BT(csp, newVar=None):
     '''Do plain backtracking propagation. That is, do no
@@ -27,4 +28,110 @@ def prop_BT(csp, newVar=None):
     return True, []
 
 def prop_FC(csp, newVar=None):
+
+    def FC_check(c, check_var):
+        vals = []
+        vars = c.get_scope()
+        for i in range(len(vars)):
+            var = vars[i]
+            if var != check_var:
+                vals.append(var.get_assigned_value())
+            else:
+                check_varIdx = i
+                vals.append(-1)
+
+        domain = check_var.cur_domain()
+        prunings = []
+        for val in domain:
+            vals[check_varIdx] = val
+            if not c.check(vals):
+                prunings.append((check_var,val))
+                check_var.prune_value(val)
+        
+        if len(prunings) == len(domain):
+            # DWO
+            return False, prunings
+        else:
+            # no DWO, prune values
+            return True, prunings
+
+    '''main FC'''
+    if not newVar:
+        return True, []
+    all_prunings = []
+    for c in csp.get_cons_with_var(newVar):
+        if c.get_n_unasgn() == 0:
+            vals = []
+            vars = c.get_scope()
+            for var in vars:
+                vals.append(var.get_assigned_value())
+            if not c.check(vals):
+                return False, []
+        elif c.get_n_unasgn() == 1:
+            unasgn_var = (set(c.get_scope()) - set([newVar])).pop()
+
+            FC_check_ok,prunings = FC_check(c, unasgn_var)
+            all_prunings = all_prunings + prunings
+            if not FC_check_ok:
+                return False, all_prunings
+    return True, all_prunings
+
+def prop_GAC(csp, newVar=None):
+
+    def GAC_enforce(GAC_q):
+        prunings = []
+        while GAC_q:
+            c = GAC_q.popleft()
+
+            vars = c.get_scope()
+            for var in vars:
+                domain = var.cur_domain()
+                for val in domain:
+                    if not c.has_support(var, val):
+                        prunings.append((var, val))
+                        var.prune_value(val)
+                        if var.cur_domain_size() == 0:
+                            # DWO
+                            GAC_q.clear()
+                            return False, prunings
+                        else:
+                            cons = csp.get_cons_with_var(var)
+                            for c in cons:
+                                if GAC_q.count(c) == 0:
+                                    GAC_q.append(c)
+        # no DWO, prune values
+        return True, prunings
+
+    '''main GAC'''
+    if not newVar:
+        return True, []
+
+    GAC_q = deque()
+    cons = csp.get_cons_with_var(newVar)
+    for c in cons:
+        GAC_q.append(c)
+
+    GAC_ok,prunings = GAC_enforce(GAC_q)
+
+    if not GAC_ok:
+        return False, prunings
+    return True, prunings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
