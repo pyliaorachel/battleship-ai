3d834f8d4979db9764dbd1c98fdd3af099068deb
Iven <ivenpoon@gmail.com>
2016-12-03 10:12:22 -0500

    Done correctness_test_engine

--- test/correctness_test_engine.py
+++ test/correctness_test_engine.py
@@ -2,14 +2,14 @@ from test.validity_check import is_valid
 from test.test_generator import BattleshipTest
 from test.utilities import *
 from csp.battleship_csp import battleship_csp_model1, battleship_csp_model2
-from csp.battleship_BT import BT
+from csp.battleship_BT import *
 from csp.propagators import *
 from csp.orderings import *
 import traceback
 
 
-def basic_check(model, prop_type, var_ord_type, val_ord_type, trace_BT=False):
-    for i in range(1, 3):
+def basic_check(model, bt, prop_type, var_ord_type, val_ord_type, trace_BT=False):
+    for i in range(1, 4):
         for j in range(1, i ** 2):
             # prepare a test
             test = BattleshipTest(generate_target_map(i, j))
@@ -24,7 +24,7 @@ def basic_check(model, prop_type, var_ord_type, val_ord_type, trace_BT=False):
             battleship_csp, variable_array = model(row_targets, col_targets, ships)
 
             try:
-                solver = BT(battleship_csp)
+                solver = bt(battleship_csp)
                 if trace_BT:
                     solver.trace_on()
                 if prop_type == 'BT':
@@ -34,10 +34,6 @@ def basic_check(model, prop_type, var_ord_type, val_ord_type, trace_BT=False):
                 elif prop_type == 'GAC':
                     solver.bt_search(prop_GAC, var_ord_type, val_ord_type)
                 solution = battleship_csp.get_sol_board()
-
-                #TODO: there is an issue with printing out the solutions... need to figure this out
-                print('{} {} {} {}'.format(model.__name__, prop_type, var_ord_type, val_ord_type))
-                print(solution)
 
                 # check if is valid
                 if not is_valid(solution, test):
@@ -56,11 +52,11 @@ def basic_check(model, prop_type, var_ord_type, val_ord_type, trace_BT=False):
 
 
 if __name__ == '__main__':
-    basic_check(battleship_csp_model1, 'BT', ord_random, val_arbitrary)
-    basic_check(battleship_csp_model1, 'BT', ord_random, val_decrease_lcv)
-    basic_check(battleship_csp_model1, 'BT', ord_random, val_decreasing_order)
-    basic_check(battleship_csp_model1, 'BT', ord_random, val_increasing_order)
-    basic_check(battleship_csp_model1, 'BT', ord_random, val_lcv)
-    basic_check(battleship_csp_model1, 'BT', ord_mrv, val_decreasing_order)
-    basic_check(battleship_csp_model1, 'BT', ord_dh, val_decreasing_order)
-    basic_check(battleship_csp_model2, 'BT', ord_random, val_arbitrary)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_random, val_arbitrary)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_random, val_decrease_lcv)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_random, val_decreasing_order)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_random, val_increasing_order)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_random, val_lcv)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_mrv, val_decreasing_order)
+    basic_check(battleship_csp_model1, battleship_BT, 'BT', ord_dh, val_decreasing_order)
+    basic_check(battleship_csp_model2, BT, 'BT', ord_random, val_arbitrary)
